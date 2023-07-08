be2e73a0e1e961475cf68cffb280dca5fdca0a56
Jitendra Kumar Singh <132134089+jitutestgithub@users.noreply.github.com>
2023-07-08 15:17:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package survey;
+
+import java.sql.SQLException;
+
+public class Main {
+	public static void main(String args[]) throws SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
+//JITENDRA KUMAR SINGH
